092d7ccbb110e5d0f2a86a5bfd91a7d4d6c2ca1b
dt10dw <martin.etherton@ing.nl>
2019-03-07 14:19:40 +0100

    added service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -49,20 +49,32 @@ const manifest = {
 
 window.addEventListener('load', () => {
     const base = document.querySelector('base');
-let baseUrl = base && base.href || '';
+    let baseUrl = base && base.href || '';
 
-if (!baseUrl.endsWith('/')) {
-    baseUrl = `${baseUrl}/`;
-}
+    if (!baseUrl.endsWith('/')) {
+        baseUrl = `${baseUrl}/`;
+    }
 
-manifest['start_url'] = `${baseUrl}index.html`;
+    manifest['start_url'] = `${baseUrl}index.html`;
 
-manifest.icons.forEach(icon => {
-    icon.src = `${baseUrl}${icon.src}`;
-});
+    manifest.icons.forEach(icon => {
+        icon.src = `${baseUrl}${icon.src}`;
+    });
 
-const stringManifest = JSON.stringify(manifest);
-const blob = new Blob([stringManifest], {type: 'application/json'});
-const manifestURL = URL.createObjectURL(blob);
-document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
+    const stringManifest = JSON.stringify(manifest);
+    const blob = new Blob([stringManifest], {type: 'application/json'});
+    const manifestURL = URL.createObjectURL(blob);
+    document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register(`${baseUrl}sw.js`)
+            .then( registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    }
+
 });

72078cb1aa047ea6bf0b814d83e98fba6c4af4c6
dt10dw <martin.etherton@ing.nl>
2019-03-07 14:59:13 +0100

    renamed dir

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activating Service Worker ...', event);
-return self.clients.claim();
-});
